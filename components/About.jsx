24e11c7f80fafa3740f4ce5f42370ea5fe2a1f57
Mamosebetsi Kekae <mkekae@gmail.com>
2022-07-06 11:08:05 -0700

    done with the navbar and started with about page

---
+++ components/About.jsx
@@ -0,0 +1,23 @@
+import {MdExpandMore} from 'react-icons/md'
+
+
+const About = () => {
+	return (
+		<div
+			className="mx-auto text-center pt-24 p-4 font-ovsyanka
+    ">
+			<h1 className="text-3xl text-white font-extrabold mt-7">
+				Summer{" "}
+				<span className="text-yellow-400 origin-center font-higelak tracking-widest font-semibold ">
+					Vibes{" "}
+				</span>
+			</h1>
+
+      <p className=" text-xs text-yellow-200 text-center font-basting">
+      Summer is the best time of the year..
+      </p>
+		</div>
+	);
+};
+
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
+
+import About from "../components/About"
+import Gallery from "../components/Gallery"
 export default function Home() {
 	return (
 
-		<>
+		
+		<About/>
 		
 		
-		</>
+		
 	)
 		
 }
