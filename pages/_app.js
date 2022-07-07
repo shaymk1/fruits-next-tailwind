14d3a93e8c4065daddce9826339daf1da47cc4de
Mamosebetsi Kekae <mkekae@gmail.com>
2022-07-07 14:59:51 -0700

    went back to the listing of all images as mapping through all of them was not working, i will come back to it

--- pages/_app.js
+++ pages/_app.js
@@ -5,10 +5,9 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
-			<div className="min-h-screen bg-slate-900">
-				<Navbar />
+			<div className="min-h-screen bg-blue-300">
 				<Component {...pageProps} />
-                <Footer/>
+				<Footer />
 			</div>
 		</>
 	);
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
 
 
 import Gallery from "../components/Gallery"
+import Navbar from "../components/Navbar"
 export default function Home() {
 	return (
-
-		
-		<Gallery/>
-		
-		
-		
-	)
+		<div>
+			<Navbar />
+			<Gallery />
+		</div>
+	);
 		
 }
